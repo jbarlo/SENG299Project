439e2a3d48a6503df34808353af7aa91f1b21241
Keiran <keiranreilly7@gmail.com>
2016-07-10 16:41:05 -0700

    fixed board

--- board.js
+++ board.js
@@ -1,25 +1,23 @@
 /*
 
 */
-class board{
-	constructor(s){
-		this.size = s;
-		this.tokenSpots = new Array(s);
-		for(n = 0; n < s; n++){
-			this.tokenSpots[n] = new Array(s);
-		}
-		for(n = 0; n < s; n++){
-			for(i = 0; i < s; i++){
-				this.tokenSpots[n][i] = 0;
-			}
+function board(s){
+	this.size = s;
+	this.tokenSpots = newArray(s);
+	for(n = 0; n < s; n++){
+		this.tokenSpots[n] = new Array(s);
+	}
+	for(n = 0; n < s; n++){
+		for(i = 0; i < s; i++){
+			this.tokenSpots[n][i] = 0;
 		}
 	}
-	
-	function placeToken(x, y, c){
-		this.tokenSpots[x][y] = c;
-	}
-	function readBoard(){
-		return this.tokenSpots;
-	}
-	
+}
+
+
+function placeToken(x, y, c){
+	this.tokenSpots[x][y] = c;
+}
+function readBoard(){
+	return this.tokenSpots;
 }
