5365932070b32343fc98a684aed293e6a3a74393
Isaac S <istreight@users.noreply.github.com>
2016-07-10 16:30:17 -0700

    Added content to front.js
    Added aesthetics and a less coupled state-draw interactions.

--- front.js
+++ front.js
@@ -1,53 +1,101 @@
-var boardSize = 4;
-for(var squares = []; squares.length < boardSize + 1;)
-    squares.push(Array(boardSize + 1).fill(0));
+var tokenA;
+var tokenB;
+var squares;
+var tokenSize;
+var squareLength;
+
+/**
+ * Requests a new board state from the server's /data route.
+ * 
+ * @param cb {function} callback to call when the request comes back from the server.
+ */
+function getData(cb) {
+    $.get("/data", function(data, textStatus, xhr) {
+        console.log("Response for /data: "+textStatus);  
+
+        // handle any errors here...
+
+        // draw the board...
+        cb(4);
+    });
+}
 
 /**
  * Draws the board to the #canvas element on the page. 
  *
  * @param state {object} - an object representing the state of the board.  
  */ 
-function drawBoard(delBoard, newSize){
-    if(delBoard) {
-        document.getElementById("canvas").innerHTML = "";
-        boardSize = newSize;
-        for(squares = []; squares.length < boardSize + 1;)
-            squares.push(Array(boardSize + 1).fill(0));
-    }
+function makeMove(boardSize) {
     var turn = true;
+    for (squares = []; squares.length < boardSize + 1;)
+        squares.push(Array(boardSize + 1).fill(0));
     
-    var canvas = $("#canvas");
-
-    var size = 580;
-    canvas.css("height", size);
-    canvas.css("width", size);
+    tokenSize = Math.min(Math.ceil(600 / (3 * boardSize)), 39);
+    squareLength = Math.round(520 / boardSize);
+    tokenA = "black";
+    tokenB = "white";
     
-    var squareLength = Math.round(size/boardSize);
-    var tokenSize = Math.ceil(size / (3 * boardSize));
-
-    var svg = $(makeSVG(size, size));
-
-    canvas.css("background-color", "white");
-    for (var i = 0; i < squareLength; i++) {
-        svg.append(makeLine(0, i*squareLength, size, i*squareLength, "black", 2));
-        svg.append(makeLine(i*squareLength, 0, i*squareLength, size, "black", 2));
-    }
+    drawBoard();
     
-    canvas.click(function(event) {
-        var c = getRelativeCoords(event);
-        var x = Math.round((c.x) / squareLength) * squareLength;
-        var y = Math.round((c.y) / squareLength) * squareLength;
-            
+    $("#canvas").click(function(event) {
+        var coords = getRelativeCoords(event);
+        var x = Math.round((coords.x - 40) / squareLength) * squareLength;
+        var y = Math.round((coords.y - 40) / squareLength) * squareLength;
+        
         if (!squares[(x/squareLength)][(y/squareLength)]) {
-            svg.append(makeCircle(x, y, tokenSize, turn ? "black" : "white"));
             squares[(x/squareLength)][(y/squareLength)] = turn ? 1 : -1;
             turn = turn ? false : true;
+            drawBoard();
         }
     });
-
-    canvas.append(svg);
 }
 
+function drawBoard() {
+    document.getElementById("canvas").innerHTML = "";
+
+    var svg = $(makeSVG(600, 600));
+    $("#canvas").css("height", 600);
+    $("#canvas").css("width", 600);
+    
+    $("#canvas").css("background-color", "white");
+    for (var i = 0; i < squares.length; i++) {
+        svg.append(makeLine(40, i*squareLength + 40, (squares.length - 1)*squareLength + 40, i*squareLength + 40, "black", 2));
+        svg.append(makeLine(i*squareLength + 40, 40, i*squareLength + 40, (squares.length - 1)*squareLength + 40, "black", 2));
+    }
+
+    for (var j = 0; j < squares.length; j++) {
+        for (var k = 0; k < squares.length; k++) {
+            if (squares[j][k] !== 0) {
+                svg.append(makeCircle(j * squareLength + 40, k * squareLength + 40, tokenSize, squares[j][k] > 0 ? tokenA : tokenB));
+             }
+        }
+    }
+    
+    $("#canvas").append(svg);
+}
+
+function boardColour(newColour) {
+    if (newColour === "rnd")
+        $("#canvas").css("background-color", "#"+((1<<24)*Math.random()|0).toString(16));
+    else
+        $("#canvas").css("background-color", newColour);
+}
+
+function tokenAColour(newColour) {
+    if (newColour === "rnd")
+        tokenA = "#"+((1<<24)*Math.random()|0).toString(16);
+    else
+        tokenA = newColour;
+    drawBoard();
+}
+
+function tokenBColour(newColour) {
+    if (newColour === "rnd")
+        tokenB = "#"+((1<<24)*Math.random()|0).toString(16);
+    else
+        tokenB = newColour;
+    drawBoard();
+}
 
 /*
  * From stackoverflow.com/questions/55677/how-do-i-get-the-coordinates-of-a-mouse-click-on-a-canvas-element
@@ -61,8 +109,20 @@ function getRelativeCoords(event) {
  */
 
 //When the user clicks on the button, toggle between hiding/showing dropdown content
-function dropList() {
-    document.getElementById("dropdown-list").classList.toggle("show");
+function dropBoardSizeList() {
+    document.getElementById("board-size-list").classList.toggle("show");
+}
+
+function dropBoardColourList() {
+    document.getElementById("board-colour-list").classList.toggle("show");
+}
+
+function dropTokenAColourList() {
+    document.getElementById("tokena-colour-list").classList.toggle("show");
+}
+
+function dropTokenBColourList() {
+    document.getElementById("tokenb-colour-list").classList.toggle("show");
 }
 
 //Close the dropdown menu if the user clicks outside of it
@@ -77,3 +137,12 @@ window.onclick = function(event) {
         }
     }
 }
+
+function init() {
+
+    // do page load things here...
+
+    console.log("Initalizing Page...."); 
+    getData(makeMove);
+}
+
