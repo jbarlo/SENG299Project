aabe3d854a9fd9203b4bb737eb811e0b3d34e29c
James Barlow <kagamijay@hotmail.com>
2016-07-19 14:02:55 -0700

    no message

--- board.js
+++ board.js
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-/*
-*/
-var m = require("move");
-
-
-function board(s){
-	this.lastMove = new move();
-	this.size = s;
-	this.tokenSpots = [];
-	for(n = 0; n < s; n++){
-		this.tokenSpots[n] = [];
-	}
-	for(n = 0; n < s; n++){
-		for(i = 0; i < s; i++){
-			this.tokenSpots[n][i] = 0;
-		}
-	}
-}
-
-function placeToken(x, y, c){
-	this.tokenSpots[x][y] = c;
-	this.lastMove.makeMove(x,y,c);
-	return this.tokenSpots[x][y];
-}
-function readToken(x, y){
-	return(this.tokenSpots[x][y];
-}
-function readBoard(){
-	return this.tokenSpots;
-}
-function cloneBoard(){
-	var array = [];
-	for(n=0; n < this.size; n++){
-		array[n] = []
-		for(i = 0; i < this.size; i++){
-			array[n][i] = readToken(n,i);
-		}
-	}
-	return array;
-}
-
-=======
-/*
-*/
 var m = require("move");
 
 
@@ -71,6 +26,3 @@ function readToken(x, y){
 function readBoard(){
 	return this.tokenSpots;
 }
-
-
->>>>>>> jamlock
