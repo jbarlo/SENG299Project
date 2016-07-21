10a6d3566f06ab849cefe0511743e5841ac9d2de
lampasaurus <gremlinpower@gmail.com>
2016-07-20 17:47:47 -0700

    new test case

--- tester.js
+++ tester.js
@@ -9,13 +9,6 @@ board = back.createGame('ai', 6);
 console.log(board.readBoard());
 
 back.connect(1, 2);
-back.getMove(board, 0, 0, 0, false, callback());
-back.getMove(board, 1, 2, 0, false, callback());
-back.getMove(board, 2, 5, 1, false, callback());
-back.getMove(board, 3, 2, 2, false, callback());
-back.getMove(board, 2, 2, 1, false, callback());
-back.getMove(board, 2, 0, 2, false, callback());
-
-console.log(board.readBoard());
-
-console.log('all tests pass :D');
+back.getMove(board, 0, 0, 1, false, function callback(board){
+	console.log(board.readBoard());
+});
